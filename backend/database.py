d8002c7c2d077583c2640659ccb0fe66b523883c
Josef Aljasmi <jo.aljasmi2001@gmail.com>
2026-02-26 16:51:10 +0100

    added a few neccessary things to get the database started

---
+++ backend/database.py
@@ -0,0 +1,23 @@
+from sqlaclhemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+import os
+from dotenv import load_dotenv
+
+#trying to load everything from .env file
+load_dotenv()
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+#connecting to the database using SQLAlchemy
+engine = create_engine(DATABASE_URL)
+#creating a session to interact with the database
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+#creating a base class for the models to inherit from
+Base = declarative_base()
+#request db session for FastApi
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
