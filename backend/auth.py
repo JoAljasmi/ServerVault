9014190d5203be5be116c163698c127873fd5bf9
Josef Aljasmi <jo.aljasmi2001@gmail.com>
2026-02-27 15:39:26 +0100

    created a full authorization with JWT and OAuth

---
+++ backend/auth.py
@@ -0,0 +1,68 @@
+from datetime import datetime, timedelta, timezone
+from passlib.context import CryptContext
+from jose import JWTError, jwt
+from fastapi import Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordBearer
+from sqlalchemy.orm import Session
+from database import get_db
+import os
+from dotenv import load_dotenv
+
+
+load_dotenv()
+
+SECRET_KEY = os.getenv("SECRET_KEY")
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 60
+
+
+# ============= Password Hashing ==============
+
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+
+def has_password(password: str) -> str:
+    """turning the password into a secure hash"""
+    return pwd_context.hash(password)
+
+def verify_password(plain_password: str, hashed_password: str) -> bool:
+    """checking if the provided password matches the stored hash"""
+    return pwd_context.verify(plain_password, hashed_password)
+
+# ============= JWT Token ==============
+
+def create_access_token(data: dict) -> str:
+    """creating a jwt token with an expiration time"""
+    to_encode = data.copy()
+    expire = datetime.now(timezone.utc) + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    to_encode.update({"exp": expire})
+    return jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+
+# ============= Get current user ==============
+
+#looking for token in auth header
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/auth/login")
+
+def get_current_user(
+        token: str = Depends(oauth2_scheme),
+        db: Session = Depends(get_db)
+):
+    """Decode the JWT token and return the user"""
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="invalid or expired token",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        user_id: int = payload.get("sub")
+        if user_id is None:
+            raise credentials_exception
+    except JWTError:
+        raise credentials_exception
+    
+    from models import User
+    user = db.query(User).filter(User.id == user_id).first()
+    if user is None:
+        raise credentials_exception
+    return user
